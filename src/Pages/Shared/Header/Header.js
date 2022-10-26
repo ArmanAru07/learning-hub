c971ea370d040181b7b42b972afbbc7b81881053
ArmanAru <armanhosen768@gmail.com>
2022-10-26 21:53:20 +0600

    header

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -13,8 +13,9 @@ const Header = () => {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav.Link href="#features">Courses</Nav.Link>
+                        <Nav.Link href="#pricing">FAQ</Nav.Link>
+                        <Nav.Link href="#pricing">Blog</Nav.Link>
                         <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">
